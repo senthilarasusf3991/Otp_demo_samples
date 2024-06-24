c84753def18f1babb81f68eb537ce2d5bd6da48b
senthilarasusf3991 <118177875+senthilarasusf3991@users.noreply.github.com>
2024-06-24 18:19:01 +0530

    Add files via upload

---
+++ OTPCrgDemo1/index.js
@@ -0,0 +1,11 @@
+var numberOtp = new ej.inputs.OtpInput({
+    value: "1234",
+    type: 'number'
+});
+numberOtp.appendTo('#numberOtp');
+
+var textOtp = new ej.inputs.OtpInput({
+    value: "e3c7",
+    type: 'text'
+});
+textOtp.appendTo("#textOtp");
